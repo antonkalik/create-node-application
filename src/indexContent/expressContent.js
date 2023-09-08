958120c3a0880935a8f000b5156c7b71dd32a585
Anton Kalik <anton@amenitiz.com>
2023-09-08 03:03:06 +0200

    feat: cors middlewares

--- src/indexContent/expressContent.js
+++ src/indexContent/expressContent.js
@@ -1,9 +1,13 @@
 const expressContent = `
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json());
+app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(PORT, () => {
--- src/indexContent/koaContent.js
+++ src/indexContent/koaContent.js
@@ -5,13 +5,12 @@ import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
 
 const port = process.env.PORT || 9999;
-
 const app = new Koa();
 const router = new Router();
 
 app.use(cors());
-app.use(router.routes());
 app.use(bodyParser());
+app.use(router.routes());
 
 app.on('error', (error: Error, ctx: ExtendableContext) => {
   console.error('SERVER_ERROR', error, ctx);
